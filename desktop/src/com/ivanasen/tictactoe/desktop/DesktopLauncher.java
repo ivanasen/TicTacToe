a3e5c5329cea9db33ed662dbe2d4f33ebdd25d46
Ivan-Asen <vankata9907@gmail.com>
2016-09-12 16:32:51 +0300

    started project

---
+++ desktop/src/com/ivanasen/tictactoe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ivanasen.tictactoe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ivanasen.tictactoe.TicTacToeMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TicTacToeMain(), config);
+	}
+}
